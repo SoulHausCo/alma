e878a9738857752ecb9d7ead63a15e203b49237f
Denny Villalobos <dnyvilla@gmail.com>
2025-01-23 23:19:32 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css'; // Import the CSS file
 import './Normalize.css'; // Import the CSS file
 
-import logo from './assets/images/logo.png'; // Import your logo
+import logo from './assets/images/images.jpg'; // Import your logo
 import whatsappIcon from './assets/images/whatsapp.webp'; // Import your Whatsapp icon
 import websiteIcon from './assets/images/web.png'; // Import your Website icon
 import instagramIcon from './assets/images/instagram.webp'; // Import your Instagram icon
@@ -13,21 +13,16 @@ const App = () => {
       <div className="logo-container">
         <img src={logo} alt="Logo" className="logo" />
       </div>
-      <p className="construction-text">Estamos echando raices 👷🏾🏗️</p>
-      <div className="links">
-        <a href="https://api.whatsapp.com/send?phone=573134523232&text=Hola%20Alma%2C%20quiero%20una%20planta%20con%20alma!" className="link">
+      <p className="construction-text">Hola <b>Estefanía Arango Garcia</b></p>
+      <p className="construction-text">Desde Alma esperamos que hoy estes muy bien</p>
+      <p className="construction-text">A través de este sitio queremos ofrecer discupas por situaciones del pasado</p>
+      <p className="construction-text">Por eso venimos acá con los ánimos de que no nos odies en secreto </p>
+      <p className="construction-text">Te queremos invitar a que pases una noche en el domo con Alma</p>
+      <p className="construction-text">🏳️ esto son señales de paz 🏳️ </p>
+
+      <a href="https://api.whatsapp.com/send?phone=573134523232&text=Hola%20Denny%2C%20acepto%20tus%20disculpas%20" className="link">
           <img src={whatsappIcon} alt="Whatsapp" className="link-icon" /> Whatsapp
         </a>
-        <a href="https://almavariegada.com" className="link">
-          <img src={websiteIcon} alt="Website" className="link-icon" /> Website
-        </a>
-        <a href="https://instagram.com/almavariegada" className="link">
-          <img src={instagramIcon} alt="Instagram" className="link-icon" /> Instagram
-        </a>
-      </div>
-      <footer>
-        <p className="footer-text">Alma variegada, plantas con alma. 💚</p>
-      </footer>
     </div>
   );
 }
